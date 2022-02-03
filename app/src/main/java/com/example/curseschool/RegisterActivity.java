1058b597b1a14ab203572bfbbe3c01ead8f61534
mbo <mariuszbbbb@interia.pl>
2022-02-03 14:46:01 +0100

    Dodanie ekranu logowania oraz ekranu rejestracji nowego użytkownika

---
+++ app/src/main/java/com/example/curseschool/RegisterActivity.java
@@ -0,0 +1,24 @@
+package com.example.curseschool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        TextView button = findViewById(R.id.alreadySigned);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
