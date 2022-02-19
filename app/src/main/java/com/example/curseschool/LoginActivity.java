e640d6b70ee293c47335052a6912ee232a236fc0
mbo <mariuszbbbb@interia.pl>
2022-02-19 17:45:14 +0100

    Sprawdzenie czy dane wpisane przy logowaniu znajdują się w bazie

--- app/src/main/java/com/example/curseschool/LoginActivity.java
+++ app/src/main/java/com/example/curseschool/LoginActivity.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import net.sourceforge.jtds.jdbc.JtdsResultSet;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,6 +22,8 @@ public class LoginActivity extends AppCompatActivity {
     private TextView loginButton;
     private EditText inputUserLogin;
     private EditText inputUserPassword;
+    private Connection connect;
+    private String connectionResult = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +59,14 @@ public class LoginActivity extends AppCompatActivity {
 
     private void validateFields() {
         initializeFields();
-        checkIfFieldsEmpty();
+        boolean isEmpty = checkIfFieldsEmpty();
+        if (!isEmpty) {
+            try {
+                validateLogin();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
     }
 
     private void initializeFields() {
@@ -57,20 +74,27 @@ public class LoginActivity extends AppCompatActivity {
         inputUserPassword = findViewById(R.id.inputUserLoginPassword);
     }
 
-    private void checkIfFieldsEmpty() {
+    private boolean checkIfFieldsEmpty() {
+        boolean isEmpty = false;
         String userEmail = inputUserLogin.getText().toString();
         String userPassword = inputUserPassword.getText().toString();
         if (userEmail.isEmpty()) {
             showError(inputUserLogin, "Nie podano adresu email!");
+            isEmpty = true;
         } else if (!userEmail.contains("@")) {
             showError(inputUserLogin, "Nieprawidłowy adres email!");
+            isEmpty = true;
         } else if (userPassword.isEmpty()) {
             showError(inputUserPassword, "Nie podano hasła!");
+            isEmpty = true;
         } else if (userPassword.length() <= 7) {
             showError(inputUserPassword, "Hasło musi mieć więcej niż 7 znaków");
+            isEmpty = true;
         } else {
-            return;
+            isEmpty = false;
         }
+
+        return isEmpty;
     }
 
     private void showError(EditText inputField, String errorMsg) {
@@ -78,5 +102,39 @@ public class LoginActivity extends AppCompatActivity {
         inputField.requestFocus();
     }
 
+    private void validateLogin() throws SQLException {
+        String userEmail = inputUserLogin.getText().toString();
+        String userPassword = inputUserPassword.getText().toString();
+        ResultSet resultSet = getResultSet(userEmail);
+        if (resultSet.next() == false) {
+            showError(inputUserLogin, "Nieprawidłowy adres email!");
+        } else {
+            do {
+                String password = resultSet.getString(5);
+                if (!userPassword.equals(password))
+                    showError(inputUserPassword, "Nieprawidłowe hasło!");
+            } while (resultSet.next());
+        }
+    }
 
-}
+        private ResultSet getResultSet (String userEmail){
+            ResultSet resultSet = null;
+            try {
+                ConnectionHelper connectionHelper = new ConnectionHelper();
+                connect = connectionHelper.getConnection();
+                if (connect != null) {
+                    String query = "Select * from users where email = '" + userEmail + "'";
+                    Statement statement = connect.createStatement();
+                    resultSet = statement.executeQuery(query);
+                } else {
+                    connectionResult = "Check Connection";
+                }
+            } catch (Exception ex) {
+                Log.e("Error :", ex.getMessage());
+            }
+
+            return resultSet;
+        }
+
+
+    }
