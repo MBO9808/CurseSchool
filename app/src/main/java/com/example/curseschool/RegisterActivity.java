28887e1d2bc233468a78d3a05e41962b27160cdb
mbo <mariuszbbbb@interia.pl>
2022-02-19 15:43:16 +0100

    Poprawki walidacji pól na formularzu rejestracji nowego konta

--- app/src/main/java/com/example/curseschool/RegisterActivity.java
+++ app/src/main/java/com/example/curseschool/RegisterActivity.java
@@ -31,6 +31,10 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void handleAlreadyHaveAccount() {
         alreadySignButton = findViewById(R.id.alreadySigned);
+        goBackToLogin();
+    }
+
+    private void goBackToLogin() {
         alreadySignButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,6 +45,10 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void handleRegister() {
         signInButton = findViewById(R.id.buttonRegisterUser);
+        register();
+    }
+
+    private void register() {
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,86 +58,63 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void checkCredentials() {
-        checkFirstName();
-        checkLastName();
-        checkEmail();
-        checkPassword();
-        checkPhoneNumber();
-        checkCity();
-        checkStreet();
-        checkPostalCode();
+        initializeFields();
+        validateFields();
     }
 
-    private void checkFirstName() {
+    private void initializeFields() {
         inputUserFirstName = findViewById(R.id.inputUserFirstName);
-        String userFirstName = inputUserFirstName.getText().toString();
-        if (userFirstName.isEmpty())
-            showError(inputUserFirstName, "Nie podano imienia!");
-    }
-
-    private void checkLastName() {
         inputUserLastName = findViewById(R.id.inputUserLastName);
-        String userFirstName = inputUserLastName.getText().toString();
-        if (userFirstName.isEmpty())
-            showError(inputUserLastName, "Nie podano nazwiska!");
-    }
-
-    private void checkEmail() {
         inputUserEmail = findViewById(R.id.inputUserEmail);
-        String userEmail = inputUserEmail.getText().toString();
-        if (userEmail.isEmpty())
-            showError(inputUserEmail, "Nie podano adresu email!");
-
-        if (!userEmail.contains("@"))
-            showError(inputUserEmail, "Nieprawidłowy adres email!");
-    }
-
-    private void checkPassword() {
         inputUserPassword = findViewById(R.id.inputUserPassword);
-        String userPassword = inputUserPassword.getText().toString();
-        if (userPassword.isEmpty())
-            showError(inputUserPassword, "Nie podano hasła!");
-
-        if (userPassword.length() < 7)
-            showError(inputUserPassword, "Hasło musi mieć więcej niż 7 znaków");
-    }
-
-    private void checkPhoneNumber() {
         inputUserPhoneNumber = findViewById(R.id.inputUserPhoneNumber);
-        String userPhoneNumber = inputUserPhoneNumber.getText().toString();
-        if (userPhoneNumber.isEmpty())
-            showError(inputUserPhoneNumber, "Nie podano numeru telefonu!");
-
-        if (userPhoneNumber.length() < 7)
-            showError(inputUserPhoneNumber, "Numer telefonu musi mieć więcej niż 9 cyfr");
-    }
-
-    private void checkCity() {
         inputUserCity = findViewById(R.id.inputUserCity);
-        String userCity = inputUserCity.getText().toString();
-        if (userCity.isEmpty())
-            showError(inputUserCity, "Nie podano miasta!");
-    }
-
-    private void checkStreet() {
         inputUserStreet = findViewById(R.id.inputUserStreet);
-        String userStreet = inputUserStreet.getText().toString();
-        if (userStreet.isEmpty())
-            showError(inputUserStreet, "Nie podano ulicy!");
-    }
-
-    private void checkPostalCode() {
         inputUserPostalCode = findViewById(R.id.inputUserPostalCode);
-        String userPostalCode = inputUserPostalCode.getText().toString();
-        if (userPostalCode.isEmpty())
-            showError(inputUserPostalCode, "Nie podano kodu pocztowego!");
-
-        if (!userPostalCode.contains("-"))
-            showError(inputUserPostalCode, "Nieprawidłowy kod pocztowy!");
     }
 
-    private void showError(EditText inputUserFirstName, String errorMsg) {
-        inputUserFirstName.setError(errorMsg);
+    private void validateFields() {
+        String userFirstName = inputUserFirstName.getText().toString();
+        String userLastName = inputUserLastName.getText().toString();
+        String userEmail = inputUserEmail.getText().toString();
+        String userPassword = inputUserPassword.getText().toString();
+        String userPhoneNumber = inputUserPhoneNumber.getText().toString();
+        String userCity = inputUserCity.getText().toString();
+        String userPostalCode = inputUserPostalCode.getText().toString();
+        String userStreet = inputUserStreet.getText().toString();
+
+        if (userFirstName.isEmpty()) {
+            showError(inputUserFirstName, "Nie podano imienia!");
+        } else if (userLastName.isEmpty()) {
+            showError(inputUserLastName, "Nie podano nazwiska!");
+        } else if (userEmail.isEmpty()) {
+            showError(inputUserEmail, "Nie podano adresu email!");
+        } else if (!userEmail.contains("@")) {
+            showError(inputUserEmail, "Nieprawidłowy adres email!");
+        } else if (userPassword.isEmpty()) {
+            showError(inputUserPassword, "Nie podano hasła!");
+        } else if (userPassword.length() < 7) {
+            showError(inputUserPassword, "Hasło musi mieć więcej niż 7 znaków");
+        } else if (userPhoneNumber.isEmpty()) {
+            showError(inputUserPhoneNumber, "Nie podano numeru telefonu!");
+        } else if (userPhoneNumber.length() < 7) {
+            showError(inputUserPhoneNumber, "Numer telefonu musi mieć więcej niż 9 cyfr");
+        } else if (userCity.isEmpty()) {
+            showError(inputUserCity, "Nie podano miasta!");
+        } else if (userStreet.isEmpty()) {
+            showError(inputUserStreet, "Nie podano ulicy!");
+        } else if (userPostalCode.isEmpty()) {
+            showError(inputUserPostalCode, "Nie podano kodu pocztowego!");
+        } else if (!userPostalCode.contains("-")) {
+            showError(inputUserPostalCode, "Nieprawidłowy kod pocztowy!");
+        } else {
+            return;
+        }
+    }
+
+    private void showError(EditText inputField, String errorMsg) {
+        inputField.setError(errorMsg);
+        inputField.requestFocus();
     }
 
 
